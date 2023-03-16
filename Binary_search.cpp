c0f2829299f292a1a8ac4b174ba4b3fa11719093
andisuantara <127962364+andisuantara@users.noreply.github.com>
2023-03-17 05:36:38 +0700

    Update Binary_search.cpp

--- Binary_search.cpp
+++ Binary_search.cpp
@@ -2,32 +2,40 @@
 using namespace std;
 int index[5],array[5];
 int a, b=0,caridt;
-int main(){
-	for(a=0;a<5;a++){
-		cout<<"masukan angka pada urutan ke ["<<a<<"]: ";
+int main()
+{
+	for(a=0;a<5;a++)
+	{
+	    cout<<"masukan angka pada urutan ke ["<<a<<"]: ";
 	    cin>>array[a];
 	}
 	cout<<endl;
 	cout<<"masukan angka yang akan di cari : ";
 	cin>>caridt;
 	
-	for(a=0;a<=5;a++){
-		if(array[a] == caridt){
-			index[b]=a;
-			b++;
-		}
+	for(a=0;a<=5;a++)
+	{
+	if(array[a] == caridt)
+	   {
+	      index[b]=a;
+	      b++;
+	   }
 	}
 	
-	if(b>0){
-		cout<<endl;
-		cout<<"data "<<caridt<< " terletak di index ke : ";
-		for(a=0;a<b;a++){
-			cout<<index[a]<<" ";
-		}
-		cout<<endl;
+	if(b>0)
+	{
+	cout<<endl;
+        out<<"data "<<caridt<< " terletak di index ke : ";
+	for(a=0;a<b;a++)
+	{
+	cout<<index[a]<<" ";
+	   }
+	      cout<<endl;
+	   }
+	else
+	{
+	cout<<" bilangan tidak di temukan";
 	}
-	else{
-		cout<<" bilangan tidak di temukan";
-	}
+	
 	return 0;
 }
